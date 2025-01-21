3d07f423669b18d8d3397553918f56bff9b059c0
Emine Oraklabel <emine.oraklabel@softwire.com>
2025-01-21 14:26:01 +0000

    read file added

--- email-extraction.js
+++ email-extraction.js
@@ -3,3 +3,14 @@
 // split text by space
 
 // for each 'block' of text, count number of times [space]string + ""
+const fs = require('fs');
+
+function readText(filePath)  {
+  fs.readFile('test.txt', 'utf-8', (err, data) => {
+    if(err) { throw err; }
+  console.log('data: ', data);
+  })};
+
+const input = fs.readText('test.txt');
+
+console.log(input);
