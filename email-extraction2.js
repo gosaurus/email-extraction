33c395ede9b6ba4086f9b2e2def26f645e90be16
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-01-25 16:18:55 +0000

    Tidier solution to Part 3

--- email-extraction2.js
+++ email-extraction2.js
@@ -8,31 +8,35 @@ function readText(filePath)  {
 
 const input = readText('test.txt');
 
-// regex expession for softwire.com
-// const re = /\b[\w\.'%+-]+[\W\S]@softwire\.com\b/g;
-// const arraySoftwire = input.match(re);
 
-// console.log(`List of ${arraySoftwire}`);
-// console.log(`Number of emails with domain <softwire.com>: ${arraySoftwire.length}`);
+const SoftwireEmails = (input) => {
+	// regex expession for softwire.com
+	const re = /\b[\w\.'%+-]+[\W\S]@softwire\.com\b/g;
+	const arraySoftwire = input.match(re);
+	return `Number of Softwire emails: ${arraySoftwire.length}`;
+}; 
 
-// regex expression for emails with all domains, including 'local@example.org.uk' and 'local@example.uk'
-const reAnyEmail = /\b[\w\.'%+-]+[\w\s\.]@{1}([\w-]+\.{1}[A-Za-z]+?[A-Za-z]*[\.]?[A-Za-z]+?)\b/g;
-// const reAnyEmail = /\w[\w\.]+\w@((\w+\.\w+)+)(?=\s)/g
-const matches = [...input.matchAll(reAnyEmail)]; //returns ITERATOR wtih matches, including capturing groups
+const DomainEmails = (input) => {
+	// regex expression for emails with all domains, including 'local@example.org.uk' and 'local@example.uk'
+	const reAnyEmail = /\b[\w\.'%+-]+[\w\s\.]@{1}([\w-]+\.{1}[A-Za-z]+?[A-Za-z]*[\.]?[A-Za-z]+?)\b/g;
+	const matches = [...input.matchAll(reAnyEmail)]; //returns ITERATOR wtih matches, including capturing groups
 
-console.log(matches.length);
 
-// Parse into dictionary
-const domains = {};
+	// Parse into dictionary
+	const domains = {};
+	for (const match of matches) {
+		const domain = match[1];
+		if (domains[domain]) {
+			domains[domain]++
+		}
+		else {
+			domains[domain] = 1;
+		}
+	};
+	return domains;
+}
 
-for (const match of matches) {
-	const domain = match[1];
-	if (domains[domain]) {
-		domains[domain]++
-	}
-	else {
-		domains[domain] = 1;
-	}
-};
 
-console.log(domains);
+// main program
+console.log(SoftwireEmails(input));
+console.log(DomainEmails(input));
