64b8878e9cae74102b1fd441012a236c33f0ba74
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-01-28 22:21:25 +0000

    Output domains depending on frequency specified

--- email-extraction2.js
+++ email-extraction2.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs');
+const readline = require('readline-sync')
 
 function readText(filePath)  {
   const data = fs.readFileSync(filePath, 'utf-8');
@@ -26,15 +27,12 @@ const domainEmails = (input) => {
 	const reAnyEmail = /\b[\w\.'%+-]+[\w\s\.]@{1}([\w-]+\.{1}[A-Za-z]+?[A-Za-z]*[\.]?[A-Za-z]+?)\b/g;
 	const matches = [...input.matchAll(reAnyEmail)]; //returns ITERATOR wtih matches, including capturing groups
 
-
-	// Parse into dictionary
 	const domainArray = [];
 	for (const match of matches) {
 		const domainRegex = match[1];
 		const keyValue = {};
 		const objInDomainArray = findDomainInArray(domainArray, domainRegex);
 		if (objInDomainArray !== undefined) {
-			// console.log(`${domainRegex} in DomainArray`);
 			objInDomainArray.count++;
 		}
 		else {
@@ -46,12 +44,29 @@ const domainEmails = (input) => {
 	return domainArray;
 }
 
-
 // main program
 console.log(`Count of all softwire.com emails:\n${softwireEmails(input)}`);
-console.log(`Count of all domains in file:\n${domainEmails(input)}`);
+console.log(`Count of all domains in file:\n${JSON.stringify(domainEmails(input))}`);
 
 // sort to most common
-const sorted = domainEmails(input);
-sorted.sort((a,b) => b.count - a.count);
-console.log(`Top 10 most common email domains:\n${sorted.slice(0,9)}`);
+const domainArray = domainEmails(input);
+const sorted = domainArray.toSorted((a,b) => b.count - a.count);
+console.log(`Top 10 most common email domains:\n`);
+console.log(sorted.slice(0,9));
+console.log("Sorted array length", sorted.length);
+
+//output all domains occuring more than given number of times
+const minNumber = parseInt(readline.question("Enter a number to show all email domains with occurrence frequency equal to that number or more: \n"));
+const filteredArray = [];
+
+for (let domainCountPairIndex = 0; domainCountPairIndex < sorted.length; domainCountPairIndex++) {
+	console.log("Domain count: ", domainCountPairIndex);
+	if (sorted[domainCountPairIndex].count >= minNumber) {
+		(console.log(`Inside if statement,${sorted[domainCountPairIndex].count}`));
+		filteredArray.push(sorted[domainCountPairIndex]);
+	}
+}
+
+console.log(`Domains with frequency >= ${minNumber}`);
+console.log(filteredArray);
+
